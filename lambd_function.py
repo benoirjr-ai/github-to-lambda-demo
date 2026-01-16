26585fc90163a915f83f01fa4acfd0088af13d4a
benoirjr-ai <benoirjr@gmail.com>
2026-01-15 22:19:19 -0800

    test

---
+++ lambd_function.py
@@ -0,0 +1,9 @@
+
+import pandas as pd 
+
+def lambda_handler(event, context):
+    d = {'col1': [1, 2, 3], 'col2': [4, 5, 6]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print(pd.__version__)
+    print('Done x1.1')
